4e8282650d227a6d87358c6c7836b3f309cf3bc0
Artem <nitroxess@gmail.com>
2022-11-01 13:21:52 +0300

    routing and endpoints

---
+++ server/controllers/brandController.js
@@ -0,0 +1,13 @@
+class BrandController {
+    async registration(req, res) {
+
+    }
+    async login(req, res) {
+
+    }
+    async check(req, res) {
+
+    }
+}
+
+module.exports = new BrandController()
